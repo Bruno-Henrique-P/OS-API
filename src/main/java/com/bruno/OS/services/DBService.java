ff4a521c6d88dbc5948807bcd540ebee0242e338
Bruno-Henrique-P <112912544+Bruno-Henrique-P@users.noreply.github.com>
2023-05-19 13:26:42 -0300

    Update DBService.java

--- src/main/java/com/bruno/OS/services/DBService.java
+++ src/main/java/com/bruno/OS/services/DBService.java
@@ -26,7 +26,7 @@ public class DBService {
 	
 	
 	
-//	public void instaciadb() {
+	public void instaciadb() {
 //		Tecnico t1 = new Tecnico(null , "TESTE com a oredm" , "700.963.070-48" , "(88) 98888-8888" );
 //		Cliente c1 = new Cliente (null , "TESTE com a ordem " ,"406.531.970-63" , "(88) 97777-7777");
 //		OS os1 = new OS(null , Prioridade.ALTA ,"apesnas um teste", Status.ANDAMENTO, t1, c1 );
@@ -38,5 +38,5 @@ public class DBService {
 //		tecnicoRepository.saveAll(Arrays.asList(t1));
 //		clienteRepository.saveAll(Arrays.asList(c1));
 //		osRepository.saveAll(Arrays.asList(os1));
-//	}
+	}
 }
